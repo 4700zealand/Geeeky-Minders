7cab9a8fb44456d4976e3461cc37ca4c6a9fe215
majidmido <71548069+majidmido@users.noreply.github.com>
2021-11-29 20:21:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
-
+import {Head} from 'next/head'
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+            <Head>
+              <meta name="GeekyMinders" content="width=device-width,minimum-scale=1, initial-scale=1" />
+            <title>Geeky Minders</title>
+        </Head>
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
