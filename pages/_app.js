24dc0ffebbea9851e741e703f748a5401f05103b
majidmido <71548069+majidmido@users.noreply.github.com>
2021-11-29 20:10:58 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,12 @@ import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return (
+    
     <Layout>
+     <Head>
+          <meta name="GeekyMinders" content="width=device-width,minimum-scale=1, initial-scale=1" />
+            <title>Geeky Minders</title>
+        </Head>
       <Component {...pageProps} />
     </Layout>
   );
