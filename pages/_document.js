d090e9687f49efc41abb1173bae6d048d6da6be2
Majid Mido <majidmido03@gmail.com>
2021-11-30 22:08:44 +0100

    main

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,11 @@ class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
-const GMD="Geeky Minders";
+
   render() {
     return (
       <Html>
-        <Head>
-          <meta name="GeekyMinders" content="width=device-width,minimum-scale=1, initial-scale=1" />
-            <title>{GMD}</title>
-        </Head>
+        <Title title="Geeky Minders" />
         <body>
           <Main />
           <NextScript />
